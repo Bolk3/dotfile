4f14f29ec3d3b6644878f7ea43c053bfc5876da1
Bolk3 <bolk3@proton.me>
2026-08-24 11:54:36 +0200

    nvim(ui): add scroll overall

--- nvim/.config/nvim/lua/plugins/ui/init.lua
+++ nvim/.config/nvim/lua/plugins/ui/init.lua
@@ -2,5 +2,6 @@ return {
 	require("plugins.ui.barbar"),
 	require("plugins.ui.noice"),
 	require("plugins.ui.lualine"),
-	require("plugins.ui.indent")
+	require("plugins.ui.indent"),
+	require("plugins.ui.neoscroll")
 }
