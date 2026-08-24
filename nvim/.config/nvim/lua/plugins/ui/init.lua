9f225494057ada3204006d265ba013d4ccb6015e
Bolk3 <bolk3@proton.me>
2026-08-24 12:32:26 +0200

    nvim(ui):add todo comment highlighting

--- nvim/.config/nvim/lua/plugins/ui/init.lua
+++ nvim/.config/nvim/lua/plugins/ui/init.lua
@@ -3,5 +3,6 @@ return {
 	require("plugins.ui.noice"),
 	require("plugins.ui.lualine"),
 	require("plugins.ui.indent"),
-	require("plugins.ui.neoscroll")
+	require("plugins.ui.neoscroll"),
+    require("plugins.ui.todo-comments"),
 }
